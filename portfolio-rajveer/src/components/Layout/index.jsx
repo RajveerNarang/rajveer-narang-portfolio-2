01ed5a59b1ecee6080cd164ed996b334fc994791
RajveerNarang <dhanurrajveer@gmail.com>
2022-11-22 19:01:54 +0530

    inital commit

---
+++ portfolio-rajveer/src/components/Layout/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss'
+import React from 'react'
+import SideBar from '../Sidebar/index'
+
+const Layout = () => {
+  return (
+    <>
+      <SideBar />
+    </>
+  )
+}
+
+export default Layout
