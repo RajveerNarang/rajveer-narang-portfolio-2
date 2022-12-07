cd4fa5b5d4c574eb5446f3aa0942a60c6692bbb4
RajveerNarang <dhanurrajveer@gmail.com>
2022-12-08 00:21:36 +0530

    Animated Letters Name

--- portfolio-rajveer/src/components/Home/Home.jsx
+++ portfolio-rajveer/src/components/Home/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../Home/Home.scss'
 import LogoR from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import AnimatedL from '../AnimatedLetters/AnimatedL'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = [
+    'a',
+    'j',
+    'v',
+    'e',
+    'e',
+    'r',
+    ' ',
+    'N',
+    'a',
+    'r',
+    'a',
+    'n',
+    'g',
+  ]
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
   return (
     <>
       <div className="container home-page">
@@ -11,7 +44,12 @@ const Home = () => {
           <h1>
             Hi, <br /> I'm
             <img src={LogoR} alt="Rajveer" />
-            ajveer Narang
+            {/* ajveer Narang */}
+            <AnimatedL
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
             <br />
             Web Developer
           </h1>
